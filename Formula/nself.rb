1c72d5862e93684b2f5a94ea3ae86fb4a4bcbb10
Aric Camarata <aric.camarata@gmail.com>
2026-05-21 17:41:55 -0400

    chore(formula): nself v1.1.4 (#13)

--- Formula/nself.rb
+++ Formula/nself.rb
@@ -1,10 +1,10 @@
 class Nself < Formula
   desc "Self-hosted backend infrastructure CLI: Postgres, GraphQL, Auth, Nginx in 5 minutes"
   homepage "https://nself.org"
-  url "https://github.com/nself-org/cli/archive/refs/tags/v1.1.3.tar.gz"
-  sha256 "319247937b10482afffdb4a9458676ff15f497771353537eda1f180d49a1be83"
+  url "https://github.com/nself-org/cli/archive/refs/tags/v1.1.4.tar.gz"
+  sha256 "359fe862691766312cfaae47e25c1e597c4dc15ff8b74623dd1080b07ea42228"
   license "MIT"
-  version "1.1.3"
+  version "1.1.4"
 
   depends_on "go" => :build
   depends_on "docker"
