f35c0a3f43aa9ddafa07a28caa6a3a3ce9ae3f06
Aric Camarata <aric.camarata@gmail.com>
2026-09-21 14:58:51 -0400

    chore(release): bump version to 1.4.6 (#79)
    Lockstep bump with cli 1.4.6.

--- Formula/nself.rb
+++ Formula/nself.rb
@@ -5,7 +5,7 @@
 class Nself < Formula
   desc "Self-hosted backend CLI: Postgres, GraphQL, Auth, Nginx in minutes"
   homepage "https://nself.org"
-  version "1.4.5"
+  version "1.4.6"
   license "MIT"
 
   depends_on "docker"
